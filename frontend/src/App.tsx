332a6f887dd358f8b3cea0df730918546217b082
Romanek <89982697+RomanGumeniuk@users.noreply.github.com>
2024-04-26 15:29:30 +0200

    Added Card from Chakra UI
    4 Simple cards from Chakra UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-
-import './App.css'
-
-import {ChakraProvider} from "@chakra-ui/react";
+import { useState } from 'react';
+import './App.css';
+import {ChakraProvider,SimpleGrid} from "@chakra-ui/react";
+import Content from './components/Content/Content'; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
        <ChakraProvider>
-        
+
+        <SimpleGrid columns={4} spacing={100}>
+        <Content/>
+        <Content/>
+        <Content/>
+        <Content/>
+        </SimpleGrid>
+
+
        </ChakraProvider>
   )
 }
