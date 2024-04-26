06a806c35a5aaed43656b06a1d76b08a1bca6bd2
Jedra Jacek <jacenty2006@gmail.com>
2024-04-26 09:08:07 -0400

    Fix

--- backend/models.py
+++ backend/models.py
@@ -3,15 +3,16 @@ from django.db import models
 
 # Create your models here.
 class Books(models.Model):
-    Title = models.CharField(max_length=100)
-    Author = models.CharField(max_length=200)
-    Price = models.FloatField()
-    Stock = models.IntegerField()
-    Image = models.CharField(max_length=2083)
-    Pages = models.IntegerField()
-    Year = models.IntegerField()
-    Description = models.TextField()
-    Language = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=200)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image = models.CharField(max_length=2083)
+    pages = models.IntegerField()
+    year = models.IntegerField()
+    description = models.TextField()
+    language = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
 
 class Cart(models.Model):
     product_pk = models.ForeignKey(Books, on_delete=models.CASCADE)
