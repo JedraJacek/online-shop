be823738faa1661e276cbf917c03b072ca00d95d
Limonkaa <mikolaj24122005@gmail.com>
2024-05-17 12:59:47 +0200

    mock-books display on site

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
-import {ChakraProvider,SimpleGrid} from "@chakra-ui/react";
-import Content from './components/Content/Content'; 
+import { useState, useEffect } from 'react';
+import { ChakraProvider, SimpleGrid } from '@chakra-ui/react';
+import Content from './components/Content/Content';
 import Navbar from './components/Navbar/Navbar';
+import booksData from './mocks/books_mock.json';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    setBooks(booksData);
+  }, []);
+
   return (
-       <ChakraProvider>
-        <Navbar></Navbar>
-          <SimpleGrid columns={4} spacing={50}>
-            <Content/>
-            <Content/>
-            <Content/>
-            <Content/>
-          </SimpleGrid>
-       </ChakraProvider>
-  )
+    <ChakraProvider>
+      <Navbar />
+      <SimpleGrid columns={4} spacing={10}>
+        {books.map((book) => (
+          <Content key={book.pk} book={book} />
+        ))}
+      </SimpleGrid>
+    </ChakraProvider>
+  );
 }
 
 export default App;
