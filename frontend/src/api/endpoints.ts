0b4edf412776a13ac91d92f8053f55e9733c16ed
RybazPolski <julorybarczyk@gmail.com>
2024-05-17 15:22:41 +0200

    I guess it'll work someday

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -1,21 +1,39 @@
 import urls from '../config/urls'
 import usersMock from '../mocks/users_mock.json'
-// import cartMock from '../mocks/cart_mock.json'
-// import addressesMock from '../mocks/addresses_mock.json'
+import cartMock from '../mocks/cart_mock.json'
+//import addressesMock from '../mocks/addresses_mock.json'
 
-export const fetchAllBooks = async () => {
+const fetchAllBooks = async () => {
     const response = await fetch(`${urls.django}/books/`);
     return await response.json();
 };
 
-export const login = async (login: string, password: string) => {
+const login = async (login: string, password: string) => {
     // const response = await fetch(`${urls.django}/...`);
     for (const user of usersMock) {
         if (user.login == login && user.password == password) {
-
+            return new Response("Zalogowano");
         }
     }
-    return new Response()
+    return new Response("Błędne dane")
     // return await response.json();
 };
 
+const updateCart = async (product : {product_pk: number, user_pk: number, count: number}) : Promise<Response> => {
+    const params = {
+      product_pk: product.product_pk,
+      user_pk: product.user_pk,
+      count: product.count,
+    };
+    const options = {
+      method: "POST",
+      body: JSON.stringify(params),
+    };
+    // return new Response() //temporaty mocked
+    return await fetch(
+      `${urls.django}/cart/update/`,
+      options
+    );
+};
+
+export { fetchAllBooks, updateCart}
