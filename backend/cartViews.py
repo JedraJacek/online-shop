ff897803901808151a6ac7d635a82f4b70f6a3b3
User Name <example@something.com>
2024-05-24 06:48:41 -0400

    some edits

--- backend/cartViews.py
+++ backend/cartViews.py
@@ -7,7 +7,6 @@ from rest_framework import generics, status
 
 class CartView(APIView):
     def get(self, request):
-        data = Cart.objects.all()
         data_with_related = Cart.objects.select_related("product_pk").all()
         serializer = CartSerializer(data_with_related, context={'request': request}, many=True)
 
@@ -15,20 +14,26 @@ class CartView(APIView):
     
 class updateCart(generics.CreateAPIView):
     def post(self, request):
+        request = request.data
         try:
-            Books.objects.get(pk = request.data.get('product_pk'))
-            data = CartAddSerializer(data=request.data)
+            cart = Cart.objects.get(user_pk = request.get("user_pk"), product_pk = request.get("product_pk"))
+            cart.count = request.get("count")
+            cart.save()
+            return Response({'success': 'Zaktualizowano koszyk'}, status=status.HTTP_200_OK)
+        except:
+            Books.objects.get(pk = request.get('product_pk'))
+            data = CartAddSerializer(data=request)
             if data.is_valid():
                 data.save()
             return Response({'success': 'Dodano do koszyka'}, status=status.HTTP_200_OK)
-        except:
-          return Response({'error': 'nie dodano przedmiotu/ow do koszyka'}, status=status.HTTP_400_BAD_REQUEST)
+            
+          
         
 class removeFromCart(generics.CreateAPIView):
     def post(self,request):
         try:
             request = request.data
-            data = Cart.objects.get(user_pk = request.get('user_pk'), product_pk = request.get('product_pk'))
+            data = Cart.objects.filter(user_pk = request.get('user_pk'), product_pk = request.get('product_pk'))
             data.delete()
 
             return Response({'success': 'Object deleted from Cart'}, status=status.HTTP_200_OK)
